e8828d4e1f4e7a122fef54bfeb9f055ff1a38a01
xRetry <marcoschwarz.at@gmail.com>
2022-03-04 14:19:25 +0100

    Unified image and temperature plot; Added line, histogram and distribution plot

--- plotting.py
+++ plotting.py
@@ -1,25 +1,51 @@
 import numpy as np
+import scipy.stats
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
-from temperature import get_square_coords, get_colors
+from temperature import _get_rect_coords, _get_colors, _get_line_coords
 
 
 def get_colormap(image: np.ndarray, bar_location: str = 'bottom'):
-    colors = get_colors(image, bar_location) 
+    colors = _get_colors(image, bar_location) 
     cm = LinearSegmentedColormap.from_list('', np.array(colors) / 255, N=len(colors))
     return cm
 
 
-def plot_image(image: np.ndarray):
-    plt.imshow(image)
+def plot_image(image: np.ndarray, colormap=None, line_points=None, point_points=None, rect_points=None):
+    if colormap is None:
+        plt.imshow(image)
+    else:
+        plt.imshow(image, cmap=colormap)
+    if point_points is not None:
+        plt.scatter(point_points[:, 0], point_points[:, 1], marker='x', c='r')
+    if line_points is not None:
+        x_line, y_line = _get_line_coords(line_points)
+        plt.plot(x_line, y_line, color='r')
+    if rect_points is not None:
+        x_rect, y_rect = _get_rect_coords(rect_points)
+        plt.plot(x_rect, y_rect, color='r')
     plt.show()
 
 
-def plot_temperatures(temperatures: np.ndarray, colormap=plt.cm.get_cmap('inferno').reversed(), point_ll=None, point_ur=None):
-    plt.imshow(temperatures, cmap=colormap)
-    if point_ll is not None and point_ur is None:
-        plt.scatter(*point_ll, marker='x', c='r')
-    if point_ll is not None and point_ur is not None:
-        x_rect, y_rect = get_square_coords(point_ll, point_ur)
-        plt.plot(x_rect, y_rect, color='r')
+def plot_line(temperatures: np.ndarray, tolerance: float = None):
+    x = np.arange(len(temperatures))
+    plt.plot(x, temperatures)
+    if tolerance is not None:
+        lower = [t-tolerance for t in temperatures]
+        upper = [t+tolerance for t in temperatures]
+        plt.fill_between(x, lower, upper, alpha=0.2)
+    plt.show()
+
+
+def plot_histogram(temperatures: np.ndarray):
+    plt.hist(temperatures.flatten(), bins=100, density=True)
+    plt.show()
+
+
+def plot_distribution(temperatures: np.ndarray, standard_uncertainty: float = 0):
+    mean = np.mean(temperatures)
+    std = np.std(temperatures.flatten())
+    t = np.linspace(mean-3*std, temperatures.max(), 200)
+    y = scipy.stats.norm.pdf(t, loc=mean, scale=std)
+    plt.plot(t, y)
     plt.show()
