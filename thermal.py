ae64e3980ad027556fc408815e24c9e52d16ad71
xRetry <marcoschwarz.at@gmail.com>
2022-03-08 16:44:35 +0100

    Added uncertainty computation

--- thermal.py
+++ thermal.py
@@ -1,6 +1,6 @@
-from typing import Tuple
+from typing import Tuple, Dict
 import numpy as np
-from scipy.interpolate import NearestNDInterpolator, RBFInterpolator, LinearNDInterpolator, interpn
+from scipy.interpolate import NearestNDInterpolator, RBFInterpolator, LinearNDInterpolator, interpn, interp2d
 
 
 def _get_colors(image: np.ndarray, bar_location: str = 'bottom') -> np.ndarray:
@@ -17,10 +17,25 @@ def _get_colors(image: np.ndarray, bar_location: str = 'bottom') -> np.ndarray:
   return colors
 
 
-def get_standard_uncertainty(tolerance: float = 0.07/2) -> float:
+def get_uncertainty(image: np.ndarray, bar_location: str, temperatures: np.ndarray, t_min: float, t_max: float, thermal_tolerance: float = 0.07) -> dict:
+  n = len(_get_colors(image, bar_location))
   # +- value in [K] for Compact XR
-  std_u = tolerance / np.sqrt(3) # square distribution: tol / sqrt(3) -> 0.68
+  u_max = 0.5 / np.sqrt(3)
+  u_min = u_max
+  i = np.arange(n)
+  u_scale = np.sqrt(((1 - i / (n-1))*u_min)**2 + ((i/(n-1)) * u_max)**2)
+  u_interp = (t_max - t_min) / (4*np.sqrt(3)*n) 
+  u = np.sqrt(u_scale**2 + u_interp**2)
+  temperature_scale = np.linspace(t_min, t_max, n)
+  mapping = dict(zip(temperature_scale, u))
+  return mapping
+  
 
+def get_line_uncertainty(uncertainties: np.ndarray, x_line, y_line) -> np.ndarray:
+  x = np.arange(uncertainties.shape[0]) 
+  y = np.arange(uncertainties.shape[1])
+  xy_line = np.column_stack([x_line, y_line])
+  return interpn((x,y), uncertainties, xy_line)
 
 
 def convert_to_temperature(image: np.ndarray, t_min: float, t_max: float, bar_location: str = 'bottom', interp: str = 'nearest'):
