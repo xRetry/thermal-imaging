5ef481b5f429105e16f067aa616da232b55a64ba
xRetry <marcoschwarz.at@gmail.com>
2022-03-08 16:42:03 +0100

    Restructured ThermalImage

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Dict
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,34 +8,42 @@ import picture, plotting, thermal
 class ThermalImage:
     _image: np.ndarray
     _temperatures: np.ndarray
-    _tolerance: float
-    _colormap: matplotlib.colors.Colormap
+    _uncertainties: np.ndarray
+    _lines: List[np.ndarray]
+    _rects: List[np.ndarray]
 
-    def __init__(self, path: str, t_min: float, t_max: float, bar_location: str = 'bottom', 
-    interp: str = 'nearest', thermal_tolerance: Optional[float] = None, colormap: str = 'original'):
-        self._image = picture.load_image(path)
-        self._temperatures = thermal.convert_to_temperature(self._image, t_min, t_max, bar_location, interp)
-        self._tolerance = thermal_tolerance / np.sqrt(3) * 2  # 95% uncertainty interval for rectangular tolerance
-        if colormap == 'original':
-            self._colormap = plotting.get_colormap(self._image, bar_location)
-        else:
-            self._colormap = plt.cm.get_cmap(colormap)
+    def __init__(self, image: np.ndarray, temperatures: np.ndarray, uncertainties: Optional[np.ndarray] = None):
+        if uncertainties is None:
+            uncertainties = np.zeros_like(temperatures)
+        self._image = image
+        self._temperatures = temperatures
+        self._uncertainties = uncertainties
+        self._lines, self._rects = [], []
 
     def plot_image(self):
         plotting.plot_image(self._image)
     
     def plot_temperatures(self):
-        plotting.plot_image(self._temperatures, self._colormap)
+        plotting.plot_image(self._temperatures)
 
-    def plot_line(self, x1: int, y1: int, x2: int, y2: int):
+    def add_line(self, x1: int, y1: int, x2: int, y2: int):
         line_points = np.array([[x1, y1], [x2, y2]])
-        _, _, z_line = picture.select_line(self._temperatures, line_points)
-        plotting.plot_image(self._temperatures, self._colormap, line_points)
-        plotting.plot_line(z_line, self._tolerance)
+        self._lines.append(line_points)
     
-    def plot_rectangle(self, x1: int, y1: int, x2: int, y2: int):
+    def add_rectangle(self, x1: int, y1: int, x2: int, y2: int):
         rect_points = np.array([[x1, y1], [x2, y2]])
-        temp_selection = picture.select_rectangle(self._temperatures, rect_points)
-        plotting.plot_image(self._temperatures, self._colormap, rect_points=rect_points)
-        plotting.plot_image(temp_selection, self._colormap)
-        plotting.plot_histogram(temp_selection)
+        self._rects.append(rect_points)
+
+    def plot_selection(self):
+        line_coords = [picture._get_line_coords(l) for l in self._lines]
+        rect_coords = [picture._get_rect_coords(r) for r in self._rects]
+        plotting.plot_image(self._temperatures, line_coords=line_coords, rect_coords=rect_coords)
+        for line in self._lines:
+            x_line, y_line, z_line = picture.select_line(self._temperatures, line)
+            tolerance = thermal.get_line_uncertainty(self._uncertainties, x_line, y_line)
+            plotting.plot_line(z_line, tolerance)
+
+        for rect in self._rects:
+            rect_select = picture.select_rectangle(self._temperatures, rect)
+            plotting.plot_image(rect_select)
+            plotting.plot_histogram(rect_select)
