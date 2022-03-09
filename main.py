c38d541287a83d77ebbec10cc2f49dea30b11c08
xRetry <marcoschwarz.at@gmail.com>
2022-03-09 23:25:17 +0100

    Added function fitting

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import numpy as np
 import picture
 
+def fit_func(x, a, b, c) -> float:
+    return a * np.exp(-b * x) + c
+
+def fit_func2(x, a, b) -> float:
+    return a + b * x
+
 def run():
-    img2 = picture.load_from_jpg(
+    img = picture.load_from_jpg(
         path='images/thermal_image.jpg',
         t_min=13,
         t_max=20,
@@ -10,18 +16,20 @@ def run():
         interp='nearest',
         thermal_tolerance=0.035,
     )
-    img = picture.load_from_tiff(
+    img2 = picture.load_from_tiff(
         path='images/tiff_sample.tiff',
         thermal_tolerance=0.035
     )
     # img.plot_image()
     # img.plot_temperatures()
-    img.add_rectangle(260, 136, 316, 77)
-    # img.add_line(416, 177, 728, 146)
+    # img.add_line(50, 2, 50, 75)
+    # img.add_rectangle(260, 136, 316, 77)
+    img.add_line(416, 177, 728, 146)
     # img.add_rectangle(710, 320, 840, 258)
-    img.plot_selection()
-    img.calibrate_selection(-10, 1)
-    img.plot_temperatures()
+    # img.plot_selection()
+    # img.calibrate_selection(-10, 1)
+    # img.plot_temperatures()
+    img.plot_selection(fit_func=fit_func2)
 
 if __name__ == '__main__':
     run()
